31a27b084f784d805abb6a48e783cdadf960b24e
Jalen Young <lital5@yahoo.com>
2024-08-10 17:54:10 -0500

    Assignment Object

--- gradebook.js
+++ gradebook.js
@@ -1,20 +1,4 @@
 //Create gradebook
-/*function copyPerson(oldPerson) {
-    const newPerson = {
-        name: {},
-        age: null,
-        location: {}
-    };
-    newPerson.name.first = oldPerson.name.first;
-    newPerson.name.last = oldPerson.name.last;
-    newPerson.age = oldPerson.age;
-    newPerson.location.city = oldPerson.location.city;
-    newPerson.location.state = oldPerson.location.state;
-    newPerson.location.zip = oldPerson.location.zip;
-    
-    return newPerson; 
-};
-*/
 var sid = 1;
 var cid = 1;
 var aid = 1;
@@ -37,22 +21,24 @@ console.log(CourseObject(cid,"JSC"))
 console.log(CourseObject(cid,"PYC"))
 console.log(CourseObject(cid,"RBC"))
 
-function AGObject(id, n, cid, wp) {
+function AGObject(id, n, cn, wp) {
     agid++
+    r = cn + "_" + n 
     let d = {
         "agid":id,
         "agname":n,
-        "courseid":cid,
+        "courseid":co[cn]["courseid"],
         "wp": wp,
         "alist":[]
     }
-    ago[n] = d
-    return "Assignment Group " + ago[n]["agname"] + " has been added for " + co[(cid-1)]
+    ago[r] = d
+    return "Assignment Group " + ago[r]["agname"] + " has been added for " + co[cn]["coursename"]
 }
-
-console.log(AGObject(agid,"KBA", 1, 0.25))
-console.log(AGObject(agid,"SBA", 1, 0.75))
-console.log(AGObject(agid,"KBA", 2, 0.25))
-console.log(AGObject(agid,"SBA", 2, 0.25))
-console.log(AGObject(agid,"KBA", 3, 0.25))
-console.log(AGObject(agid,"SBA", 3, 0.25))
+console.log(co)
+console.log(AGObject(agid,"KBA", "JSC", 0.25))
+console.log(AGObject(agid,"SBA", "JSC", 0.75))
+console.log(AGObject(agid,"KBA", "PYC", 0.25))
+console.log(AGObject(agid,"SBA", "PYC", 0.75))
+console.log(AGObject(agid,"KBA", "RBC", 0.25))
+console.log(AGObject(agid,"SBA", "RBC", 0.75))
+console.log(ago)
