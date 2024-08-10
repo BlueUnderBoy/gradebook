1d0774b1486c4adc96bf52b21ccd1710660f0a40
Jalen Young <lital5@yahoo.com>
2024-08-10 13:45:09 -0500

    troisième acte

--- gradebook.js
+++ gradebook.js
@@ -1,1 +1,34 @@
 //Create gradebook
+/*function copyPerson(oldPerson) {
+    const newPerson = {
+        name: {},
+        age: null,
+        location: {}
+    };
+    newPerson.name.first = oldPerson.name.first;
+    newPerson.name.last = oldPerson.name.last;
+    newPerson.age = oldPerson.age;
+    newPerson.location.city = oldPerson.location.city;
+    newPerson.location.state = oldPerson.location.state;
+    newPerson.location.zip = oldPerson.location.zip;
+    
+    return newPerson; 
+};
+*/
+var sid = 1;
+var cid = 1;
+var aid = 1;
+var agid = 1;
+function CourseObject(id,n) {
+    cid++
+    n = {
+        courseid:id,
+        coursename:n
+    }
+    return n
+}
+
+console.log(CourseObject(cid,"JSC"))
+console.log(CourseObject(cid,"PYC"))
+console.log(CourseObject(cid,"RBC"))
+
