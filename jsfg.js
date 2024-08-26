fe2565c2a6db26d937dc5c4c1c2b452307c8a8dd
Jalen Young <lital5@yahoo.com>
2024-08-26 18:54:59 -0500

    308_Update

--- jsfg.js
+++ jsfg.js
@@ -78,15 +78,14 @@ const CourseInfo = {
 
   function possible_points(id){
     for (b=0;b<AssignmentGroup.length;b++){
-      if (AssignmentGroup[b]["id"] == id){
-        return AssignmentGroup[b]["points_possible"]
+      if (AssignmentGroup["assignments"][b]["id"] == id){
+        return AssignmentGroup["assignments"][b]["points_possible"]
       }
       else{continue}
     }
   }
 
   function getLearnerData(CourseInfo, AssignmentGroup, [LearnerSubmission]) {
-    let avg = 0
     let dopr = 0
     let dtpr = 0
     let dopp = 0
@@ -96,24 +95,27 @@ const CourseInfo = {
     
     let d1 = {
       "id": 125,
-        "avg":avg
-        //aid: score on assignment
+      "avg":0
     }
 
     let d2 = {
-        "id": 132,
-        "avg":avg
-        //aid: score on assignment
+      "id": 132,
+      "avg":0
     }
 
     for(i=0;i<LearnerSubmission.length;i++){
       if (LearnerSubmission[i]["learner_id"] == d1["id"]){
-          if (date>AssignmentGroup["assignments"][i]["due_at"] && AssignmentGroup["assignments"][i]["due_at"] >= LearnerSubmission[i]["submission"]["submitted_at"]) {
+        for(a=0;a<AssignmentGroup.length;a++){
+          if (AssignmentGroup["submissions"][a]["id"] == LearnerSubmission[i]["assignment_id"])
+            const AGDO = new Date(AssignmentGroup["assignments"][i]["due_at"])
+            const LSDO = new Date(LearnerSubmission[i]["submission"]["submitted_at"])
+        }
+          if (date>AGDO && AGDO >= LSDO) {
               d1[LearnerSubmission[i]["assignment_id"]] = (LearnerSubmission[i]["submission"]["score"])/possible_points(LearnerSubmission[i]["assignment_id"])
               dopr += LearnerSubmission[i]["submission"]["score"]
               dopp += possible_points(LearnerSubmission[i]["assignment_id"])
           }
-          else if (date>AssignmentGroup["assignments"][i]["due_at"]) {
+          else if (date>AGDO) {
             d1[LearnerSubmission[i]["assignment_id"]] = (LearnerSubmission[i]["submission"]["score"]*0.9)/possible_points(LearnerSubmission[i]["assignment_id"])
             dopr += LearnerSubmission[i]["submission"]["score"]*0.9
             dopp += possible_points(LearnerSubmission[i]["assignment_id"])
@@ -121,12 +123,12 @@ const CourseInfo = {
           else {continue}
       }
       else {
-          if (date>AssignmentGroup["assignments"][i]["due_at"] && AssignmentGroup["assignments"][i]["due_at"] >= LearnerSubmission[i]["submission"]["submitted_at"]) {
+          if (date>AssignmentGroup["assignments"][i]["due_at"].getTime() && AssignmentGroup["assignments"][i]["due_at"].getTime() >= LearnerSubmission[i]["submission"]["submitted_at"].getTime()) {
             d2[LearnerSubmission[i]["assignment_id"]] = LearnerSubmission[i]["submission"]["score"]/possible_points(LearnerSubmission[i]["assignment_id"])
             dtpr += LearnerSubmission[i]["submission"]["score"]
             dtpp += possible_points(LearnerSubmission[i]["assignment_id"])
           }
-          else if (date>AssignmentGroup["assignments"][i]["due_at"]) {
+          else if (date>AssignmentGroup["assignments"][i]["due_at"].getTime()) {
             d2[LearnerSubmission[i]["assignment_id"]] = (LearnerSubmission[i]["submission"]["score"]*0.9)/possible_points(LearnerSubmission[i]["assignment_id"])
             dtpr += LearnerSubmission[i]["submission"]["score"]*0.9
             dtpp += possible_points(LearnerSubmission[i]["assignment_id"])
@@ -134,32 +136,12 @@ const CourseInfo = {
           else {continue}
       }
   }
-
-
+  d1["avg"] = dopr/dopp
+  d2["avg"] = dtpr/dtpp
+  return d1["avg"] + "\n" + d2["avg"]
 }
-
   
-  /*function getLearnerData(course, ag, submissions) {
-    // here, we would process this data to achieve the desired result.
-    const result = [
-      {
-        id: 125,
-        avg: 0.985, // (47 + 150) / (50 + 150)
-        1: 0.94, // 47 / 50
-        2: 1.0, // 150 / 150
-      },
-      {
-        id: 132,
-        avg: 0.82, // (39 + 125) / (50 + 150)
-        1: 0.78, // 39 / 50
-        2: 0.833, // late: (140 - 15) / 150
-      },
-    ];
+const result = getLearnerData(CourseInfo, AssignmentGroup, [LearnerSubmissions]);
   
-    return result;
-  }*/
-  
-  const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
-  
-  console.log(result);
+console.log(result);
   
